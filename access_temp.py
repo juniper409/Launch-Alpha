979024552860dac834ea29688826fa791569642e
juniper409 <mrjuniper409@gmail.com>
2020-01-19 17:28:42 -0500

    Adding access template.

---
+++ access_temp.py
@@ -0,0 +1,12 @@
+'''
+This information must be filled out in order to connect to the Twitter API.
+Tokens can be obtained by going to developer.twitter.com
+Rename this file to "access.py" and place it (if not already) in the same folder as the rest of the code.
+
+'''
+
+
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
