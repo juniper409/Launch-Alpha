58824f17f54ab000a4d027f9297aa814e59d1050
juniper409 <mrjuniper409@gmail.com>
2020-01-19 19:18:38 -0500

    renaming

--- launch_tweet.py
+++ launch_tweet.py
@@ -6,26 +6,72 @@ GitLab:juniper409
 Contact: mrjuniper409@gmail.com
 --------------------------------------------------------------------------------------------
 """
-
-import tweepy
 import access
- 
+import os
+import sys
+import time
+import tweepy
+
+
+
+line = '-'*10
+
 auth = tweepy.OAuthHandler(access.consumer_key, access.consumer_secret)
 auth.set_access_token(access.access_token, access.access_token_secret)
 api = tweepy.API(auth)
 user = api.get_user
 
-class SimpleListener(tweepy.StreamListener):
-    def on_status(self, status):
-        print(status.text)
-    
-    def on_error(self, status_code):
-        if status_code == 420:
-            return False
-        else:
-            return True
+
+while True:
 
 
-tweepy_listener = SimpleListener()
-tweepy_stream = tweepy.Stream(auth = api.auth, listener=tweepy_listener)
-tweepy_stream.filter(track=['spacex'])
+    #SpaceX
+    def spacex_tweet():
+        spacex = api.get_user('34743251', number_of_tweets = 2)
+        print(line + ' SPACEX ' + str(spacex.status.created_at) + line)
+        print(spacex.status.text + '\n')
+    #Spaceflight Now
+    def sfn_tweet():
+        sfn = api.get_user('17217640', number_of_tweets = 2)
+        print(line + ' SPACEFLIGHT NOW ' + line)
+        print(sfn.status.text + '\n')
+
+    #SpaceX Fleet
+    def fleet_tweet():
+        fleet = api.get_user('1011352897144180737', number_of_tweets = 2)
+        print(line + ' SPACEX FLEET ' + str(fleet.status.created_at) + line)
+        print(fleet.status.text + '\n')
+
+    def elon_tweet():
+        elon = api.get_user('44196397', number_of_tweets = 2)
+        print(line + ' ELON MUSK ' + str(elon.status.created_at) + line)
+        print(elon.status.text + '\n')
+
+    def countdown():
+        while t:
+            mins, sec = divmod(t, 60)
+            timer = '{:00}:{:120}'.format(mins, sec)
+            print(timer, end='\r')
+            time.sleep(120)
+        print('Refreshing...')
+
+    spacex_tweet()
+    sfn_tweet()
+    fleet_tweet()
+    elon_tweet()
+    #time.sleep(300)
+
+#Countdown to refresh
+    def countdown(t):
+        while t > 0:
+            sys.stdout.write('\rNext Refresh : {}s'.format(t))
+            t -= 1
+            sys.stdout.flush()
+            time.sleep(1)
+
+    countdown(300)
+
+
+    os.system('clear' if os.name == 'nt' else 'clear')
+else:
+    print('Something went wrong')
