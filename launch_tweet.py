5496f165e13f1ced3c21d5dc8755478e54e63b3a
juniper409 <mrjuniper409@gmail.com>
2020-01-20 08:58:18 -0500

    Adding additional launch providers.

--- launch_tweet.py
+++ launch_tweet.py
@@ -12,6 +12,13 @@ import sys
 import time
 import tweepy
 
+while True:
+#Ask how many seconds for refresh.
+    try:
+        refresh = int(input("How many seconds between refresh?: "))
+        break
+    except:
+        print("Please enter a number")
 
 
 line = '-'*10
@@ -24,42 +31,65 @@ user = api.get_user
 
 while True:
 
-
-    #SpaceX
-    def spacex_tweet():
-        spacex = api.get_user('34743251', number_of_tweets = 2)
-        print(line + ' SPACEX ' + str(spacex.status.created_at) + line)
-        print(spacex.status.text + '\n')
-    #Spaceflight Now
+#-----------------MISC-----------------
+#Spaceflight Now
     def sfn_tweet():
         sfn = api.get_user('17217640', number_of_tweets = 2)
         print(line + ' SPACEFLIGHT NOW ' + line)
         print(sfn.status.text + '\n')
 
+#-----------------SPACEX-----------------
+    #SpaceX
+    def spacex_tweet():
+        spacex = api.get_user('34743251', number_of_tweets = 2)
+        print(line + ' SPACEX OFFICIAL ' + line)
+        print(spacex.status.text + '\n')
+
     #SpaceX Fleet
     def fleet_tweet():
         fleet = api.get_user('1011352897144180737', number_of_tweets = 2)
-        print(line + ' SPACEX FLEET ' + str(fleet.status.created_at) + line)
+        print(line + ' SPACEX FLEET ' + line)
         print(fleet.status.text + '\n')
 
+    #Elon Musk (Founder/CEO, SpaceX)
     def elon_tweet():
         elon = api.get_user('44196397', number_of_tweets = 2)
-        print(line + ' ELON MUSK ' + str(elon.status.created_at) + line)
+        print(line + ' ELON MUSK ' + line)
         print(elon.status.text + '\n')
 
-    def countdown():
-        while t:
-            mins, sec = divmod(t, 60)
-            timer = '{:00}:{:120}'.format(mins, sec)
-            print(timer, end='\r')
-            time.sleep(120)
-        print('Refreshing...')
+
+#-----------------UNITED LAUNCH ALLIANCE-----------------
+    #United Launch Alliance
+    def ula_tweet():
+        ula = api.get_user('22845770')
+        print(line + 'UNITED LAUNCH ALLIANCE OFFICIAL' + LINE)
+        print(ula.status.text + '\n')
+
+
+    #Tory Bruno (CEO, ULA)
+    def bruno_tweet():
+        bruno = api.get_user('2882413844')
+        print(line + 'ROCKETLAB OFFICIAL' + LINE)
+        print(bruno.status.text + '\n')
+
+#-----------------Rocket Lab-----------------
+    #Rocket Lab
+    def rocketlab_tweet():
+        rocketlab = api.get_user('91145174')
+        print(line + 'ROCKETLAB' + LINE)
+        print(rocketlab.status.text + '\n')
+
+    #Peter Beck (Founder/CEO, Rocketlab)
+    def peterbeck_tweet():
+        peterbeck = api.get_user('976574172468936704')
+        print(line + 'ROCKETLAB' + LINE)
+        print(peterbeck.status.text + '\n')
+    
 
     spacex_tweet()
     sfn_tweet()
     fleet_tweet()
     elon_tweet()
-    #time.sleep(300)
 
 #Countdown to refresh
     def countdown(t):
@@ -69,7 +99,7 @@ while True:
             sys.stdout.flush()
             time.sleep(1)
 
-    countdown(300)
+    countdown(int(refresh))
 
 
     os.system('clear' if os.name == 'nt' else 'clear')
